51051e8548136461a117f9c58e1107f5949d75cb
unknown <aztoorabally7447@gmail.com>
2022-08-24 10:20:23 +0200

    'update'

--- src/api.js
+++ src/api.js
@@ -1,14 +1,12 @@
-
 const express = require("express");
 const serverless = require("serverless-http");
 
 const router = express.Router();
-const cors = require("cors"); 
+const cors = require("cors");
 const app = express();
-app.set("port", process.env.PORT ||6969); 
+app.set("port", process.env.PORT || 6969);
 app.use(express.json());
 app.use(cors());
-
 
 const userRoute = require("../routes/userRoute");
 const userPostRoute = require("../routes/userPostRoute");
@@ -16,7 +14,7 @@ const userMessageRoute = require("../routes/userMessageRoute");
 const userFriendRoute = require("../routes/userFriendRoute");
 const groupRoute = require("../routes/groupRoute");
 const groupPostRoute = require("../routes/groupPostRoute");
-// const groupMetaRoute = require("../routes/groupMetaRoute");
+const groupMettRoute = require("../routes/groupMetaRoute");
 const groupMessageRoute = require("../routes/userMessageRoute");
 const groupMemberRoute = require("../routes/groupMemberRoute");
 
@@ -27,7 +25,7 @@ app.listen(app.get("port"), () => {
 
 router.get("/", (req, res) => {
   res.json({
-   msg:`welcome to the Terra Scientia`
+    msg: `welcome to the Terra Scientia`,
   });
 });
 app.use(`/.netlify/functions/api`, router);
@@ -38,7 +36,7 @@ router.use("/userMessage", userMessageRoute);
 router.use("/userFriend", userFriendRoute);
 router.use("/group", groupRoute);
 router.use("/groupPost", groupPostRoute);
-// router.use("/groupMeta", groupMetaRoute);
+ router.use("/groupMeta", groupMettRoute);
 router.use("/groupMessage", groupMessageRoute);
 router.use("/groupMember", groupMemberRoute);
 
