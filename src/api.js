1aba299c79b5aa229bdd0987dbf52c6c616cb732
unknown <aztoorabally7447@gmail.com>
2022-08-24 10:05:51 +0200

    'routes'

--- src/api.js
+++ src/api.js
@@ -11,7 +11,14 @@ app.use(cors());
 
 
 const userRoute = require("../routes/userRoute");
-
+const userPostRoute = require("../routes/userPostRoute");
+const userMessageRoute = require("../routes/userMessageRoute");
+const userFriendRoute = require("../routes/userFriendRoute");
+const groupRoute = require("../routes/groupRoute");
+const groupPostRoute = require("../routes/groupPostRoute");
+const groupMetaRoute = require("../routes/groupMetaRoute");
+const groupMessageRoute = require("../routes/userMessageRoute");
+const groupMemberRoute = require("../routes/groupMemberRoute");
 
 app.listen(app.get("port"), () => {
   console.log(`Listening for calls on port ${app.get("port")}`);
@@ -23,10 +30,17 @@ router.get("/", (req, res) => {
    msg:`welcome to the Terra Scientia`
   });
 });
-
 app.use(`/.netlify/functions/api`, router);
 
 router.use("/users", userRoute);
+router.use("/userPost", userPostRoute);
+router.use("/userMessage", userMessageRoute);
+router.use("/userFriend", userFriendRoute);
+router.use("/group", groupRoute);
+router.use("/groupPost", groupPostRoute);
+router.use("/groupMeta", groupMetaRoute);
+router.use("/groupMessage", groupMessageRoute);
+router.use("/groupMember", groupMemberRoute);
 
 module.exports = app;
 module.exports.handler = serverless(app);
